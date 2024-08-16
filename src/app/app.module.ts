8904f79b29554179f588ce1589431b01fcb7cd58
Willy Cruz <wacb14@gmail.com>
2024-08-16 16:17:55 -0500

    About page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, MainPageComponent, HomePageComponent],
+  declarations: [AppComponent, NavbarComponent, MainPageComponent, HomePageComponent, AboutPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
