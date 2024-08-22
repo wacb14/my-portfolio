6f79253559fed7421ca06722aa08bf983bbdff8d
Willy Cruz <wacb14@gmail.com>
2024-08-22 18:03:16 -0500

    Skills icons order

--- src/app/components/skills-page/skills-page.component.ts
+++ src/app/components/skills-page/skills-page.component.ts
@@ -11,29 +11,29 @@ export class SkillsPageComponent {
     { file: 'html.svg', title: 'HTML' },
     { file: 'css.svg', title: 'CSS' },
     { file: 'js.svg', title: 'JavaScript' },
-    { file: 'angular.svg', title: 'Angular' },
     { file: 'ts.svg', title: 'TypeScript' },
+    { file: 'angular.svg', title: 'Angular' },
   ];
   backend = [
     { file: 'csharp.svg', title: 'C#' },
     { file: 'netcore.svg', title: '.NET Core' },
+    { file: 'nodejs.svg', title: 'NodeJS' },
     { file: 'mysql.svg', title: '.MySQL' },
     { file: 'sqlserver.svg', title: 'SQL Server' },
     { file: 'mongodb.svg', title: 'MongoDB' },
-    { file: 'nodejs.svg', title: 'NodeJS' },
   ];
   others = [
     { file: 'linux.svg', title: 'Linux' },
     { file: 'python.svg', title: 'Python' },
-    { file: 'nginx.svg', title: 'Nginx' },
     { file: 'git.svg', title: 'Git' },
     { file: 'github.svg', title: 'Github' },
+    { file: 'azure.svg', title: 'Azure DevOps' },
     { file: 'vscode.svg', title: 'Visual Studio Code' },
+    { file: 'docker.svg', title: 'Docker' },
+    { file: 'nginx.svg', title: 'Nginx' },
+    { file: 'apache.svg', title: 'Apache' },
+    { file: 'xd.svg', title: 'Adobe XD' },
     { file: 'photoshop.svg', title: 'Adobe Photoshop' },
     { file: 'illustrator.svg', title: 'Adobe Illustrator' },
-    { file: 'xd.svg', title: 'Adobe XD' },
-    { file: 'docker.svg', title: 'Docker' },
-    { file: 'apache.svg', title: 'Apache' },
-    { file: 'azure.svg', title: 'Azure DevOps' },
   ];
 }
