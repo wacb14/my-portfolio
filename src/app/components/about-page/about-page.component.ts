b475519534752eb7ee4af0c46e03c36bc48e7e66
Willy <wacb14@gmail.com>
2024-11-20 15:12:47 -0500

    About page information complete

--- src/app/components/about-page/about-page.component.ts
+++ src/app/components/about-page/about-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-about-page',
-  templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.css']
+    selector: 'app-about-page',
+    templateUrl: './about-page.component.html',
+    styleUrls: ['./about-page.component.css'],
 })
 export class AboutPageComponent {
-
+    iconsPath = '../../../assets/icons/';
 }
