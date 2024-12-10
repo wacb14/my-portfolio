ef8da61639279319859dbab315e5d7a7f310f0d0
Willy <wacb14@gmail.com>
2024-12-10 15:09:23 -0500

    Fix mergin conflicts

--- src/app/components/projects-page/projects-page.component.ts
+++ src/app/components/projects-page/projects-page.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects-page',
   templateUrl: './projects-page.component.html',
-  styleUrls: ['./projects-page.component.css']
+  styleUrls: ['./projects-page.component.css'],
 })
 export class ProjectsPageComponent {
-
+  iconsRoute = '../../../assets/icons/';
 }
